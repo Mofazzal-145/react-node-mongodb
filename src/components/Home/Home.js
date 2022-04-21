5701afcdaab3eb3788cfb4463ea348acdc7ef6ce
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-22 02:04:47 +0600

    try to update item

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -6,7 +7,28 @@ const Home = () => {
         fetch('http://localhost:5000/user')
         .then(res => res.json())
         .then(data => setUsers(data))
-    },[])
+    },[]);
+
+    const handelUserDelete = id => {
+        const proceed = window.confirm('Are you sure you want to delete?');
+        if(proceed){
+            console.log('delete',id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0) {
+                    console.log('deleted');
+                    const remaining = users.filter(user => user._id !== id);
+                    setUsers(remaining);
+                }
+            })
+        }
+    }
+
+
     return (
 
         <div>
@@ -16,7 +38,8 @@ const Home = () => {
                     users.map(user => <li
                     key={user._id}
                     >{user.name}:: {user.email}
-                    <button>Delete</button>
+                    <Link to={`/update/${user._id}`}><button>Update</button></Link>
+                    <button onClick={() => handelUserDelete(user._id)}>Delete</button>
                     </li>)
                     
                 }

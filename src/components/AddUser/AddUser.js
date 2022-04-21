c578e4b98960512faf5b559b3e74a5c2bb1ebee1
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-22 00:58:58 +0600

    update the home part

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
 const AddUser = () => {
-    // const handleAddUser = event => {
-    //     event.preventDefault();
+    const handleAddUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
 
-   // }
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success',data);
+            alert('users alert successfully');
+            event.target.reset();
+        })
+    }
     return (
-       <h2>This is user</h2>
+        <div onSubmit={handleAddUser}>
+            <h2>Please add a new user!!!!</h2>
+            <form action="">
+                <input type="text" name="name" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" placeholder='Email' required />
+                <br />
+                <input type="submit" value='Add User'/>
+            </form>
+        </div>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect( () =>{
+        fetch('http://localhost:5000/user')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    },[])
     return (
+
         <div>
-            <h2>This is home</h2>
+            <h2>The number of users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                    key={user._id}
+                    >{user.name}:: {user.email}
+                    <button>Delete</button>
+                    </li>)
+                    
+                }
+            </ul>
         </div>
     );
 };

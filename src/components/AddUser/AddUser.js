d1e4bc85082c107e63fb4964bd8c28a60c95628e
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-21 20:40:39 +0600

    node with mongodb

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddUser = () => {
+    // const handleAddUser = event => {
+    //     event.preventDefault();
+
+   // }
+    return (
+       <h2>This is user</h2>
+    );
+};
+
+export default AddUser;
